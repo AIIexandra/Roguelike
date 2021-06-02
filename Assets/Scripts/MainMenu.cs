f705c5ebd25c0a4c82533ee6640720facb6cf4e7
AIIexandra <9046313266@mail.ru>
2021-06-02 16:45:49 +0300

    MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnPlayScene()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void OnRecordsScene()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/RecordsScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RecordsScene : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("Scene1 is load");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnMainMenu()
+    {
+        Debug.Log("Scene2 is load");
+        SceneManager.LoadSceneAsync(0);
+    }
+}
