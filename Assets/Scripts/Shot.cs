075188975d5e562169528e5d6077524e4c6d1549
AIIexandra <9046313266@mail.ru>
2021-06-04 23:02:36 +0300

    Shot

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    public Transform bullet;
+    public int bulletForse = 5000;
+    //public int magazine = 100;
+    public AudioClip shot;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Transform bulletInstance = (Transform)Instantiate(bullet, GameObject.Find("SpawnBullet").transform.position, Quaternion.identity);
+            bulletInstance.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForse);
+            GetComponent<AudioSource>().PlayOneShot(shot);
+        }
+    }
+}
---
+++ Assets/Scripts/Shot2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot2 : MonoBehaviour
+{
+    public ParticleSystem shotEffect;    //эффект на дуле
+    public GameObject hitEffect;         //эфеект после выстрела
+    public float distance = 15;          //дальность выстрела
+    public GameObject spawn;             //место, откуда RaycastHit
+    public Transform spawnBullet;        //место, откуда пули
+    public AudioSource shotSoundSource;  //звук выстрела
+    public AudioClip shotSoundClip;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shot();
+        }
+    }
+
+    void Shot()
+    {
+        shotSoundSource.PlayOneShot(shotSoundClip);
+        //Instantiate(shotEffect, spawnBullet.position, spawnBullet.rotation);
+        //shotEffect.Play();
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(spawn.transform.position, spawn.transform.forward, out hit, distance))
+        {
+            GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impact, 2f);
+
+            if (hit.rigidbody != null)
+            {
+
+            }
+        }
+    }
+}
