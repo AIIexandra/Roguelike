45ab46f9473f42899a2c9f2cd789416c8e8c0511
AIIexandra <9046313266@mail.ru>
2021-06-10 22:54:22 +0300

    BadGuy following

--- Assets/Scripts/BadGuy.cs
+++ Assets/Scripts/BadGuy.cs
@@ -1,19 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BadGuy : MonoBehaviour
 {
+    public float currentHealth = 50;  //здоровье
 
-    public float currentHealth = 50;
+    public GameObject player;
+    public float distance;
+    NavMeshAgent nav;
+    public float radius = 15;
 
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void FixedUpdate()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+
+        if(distance > radius || distance < 2)
+        {
+            nav.enabled = false;
+        }
+
+        else
+        {
+            nav.enabled = true;
+            nav.SetDestination(player.transform.position);
+        }
+    }
+    
     public void Damage(float damageAmount)
     {
         currentHealth -= damageAmount;
 
         if(currentHealth <= 0)
         {
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
+            Destroy(gameObject);        
         }
     }
 }
