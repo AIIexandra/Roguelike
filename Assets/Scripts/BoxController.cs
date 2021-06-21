e89a56b4b19f64a67d30d894ccece009e1a46108
AIIexandra <9046313266@mail.ru>
2021-06-21 21:57:48 +0300

    Spawn boxes

---
+++ Assets/Scripts/BoxController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    public float distance;
+    public Animation animationMoney;
+    public Animation animationMed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
+            {
+                if (hit.collider.tag == "BoxMoney")
+                {
+                    animationMoney = hit.collider.GetComponent<Animation>();
+                    animationMoney.Play();
+                }
+
+                if (hit.collider.tag == "BoxMed")
+                {
+                    animationMed = hit.collider.GetComponent<Animation>();
+                    animationMed.Play();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnBoxes.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBoxes : MonoBehaviour
+{
+
+    public GameObject[] boxTypes;
+    public Transform spawn;
+
+    [Range(0, 1)]
+    public float chance = 0.2f;
+
+    public bool spawned = false;
+    int i = 0;
+
+    private void Start()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        i++;
+        double rand = Random.value;
+
+        if (other.tag == "Lock" && rand <= chance && spawned == false && i >= 6)
+        {
+            GameObject boxType = boxTypes[Random.Range(0, boxTypes.Length)];
+            Instantiate(boxType, spawn.position, spawn.rotation);
+            Destroy(gameObject);
+            spawned = true;
+        }
+
+        if (i >= 6)
+        {
+            spawned = true;
+            Destroy(gameObject);
+        }
+    }
+}
